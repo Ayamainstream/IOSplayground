2d12e184a3a6ba4cbe6b01e763d0a84b75a7409c
Aiya Zeinulla <72498812+Ayamainstream@users.noreply.github.com>
2024-08-02 10:19:05 +0500

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+print("Я сегодня изучил: ")
+print("Основы языка Swift")
+print("Как использовать Playgrounds")
+
+print("About my favourite song and singer/band: ")
+print("Favourite band \"Imagine Dragons\"")
+print("Song \"Bad Liar\"")
